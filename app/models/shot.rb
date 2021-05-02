bf30ee5dc8a7655bf5cefd9933abba745ab7a748
ShinJaehun <gunsmoke.shin@gmail.com>
2021-05-03 00:32:05 +0900

    js를 이용한 dropzone을 구현했으나 실패

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,3 +1,12 @@
 class Shot < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :user_shot
+
+  def full
+    return self.user_shot.variant(resize: '800x600')
+  end
+  def thumb
+    return self.user_shot.variant(resize: '400x300')
+  end
 end
